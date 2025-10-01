0875d3fd536d7d41105346a172c4ad19359c8725
gu1d0vsk <146665367+gu1d0vsk@users.noreply.github.com>
2025-10-01 15:17:19 -0300

    Update mensagens.py

--- mensagens.py
+++ mensagens.py
@@ -1,7 +1,9 @@
 import datetime
+import pytz
 
 def obter_mensagem_do_dia():
     """Retorna uma mensagem engraçada baseada no dia da semana."""
+    fuso_horario_brasil = pytz.timezone("America/Sao_Paulo")
     hoje = datetime.date.today().weekday() # Segunda é 0 e Domingo é 6
     mensagens = {
         0: "Segundou, EBA!!",
